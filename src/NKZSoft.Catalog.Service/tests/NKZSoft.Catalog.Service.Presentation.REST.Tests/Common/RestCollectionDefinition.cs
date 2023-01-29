0c002d4a4d748e955a178adb8c9874e636beb164
nkz <nczsoft@gmail.com>
2023-01-29 11:17:45 +0300

    Delete src/NKZSoft.Catalog.Service/tests/NKZSoft.Catalog.Service.Presentation.REST.Tests directory

--- src/NKZSoft.Catalog.Service/tests/NKZSoft.Catalog.Service.Presentation.REST.Tests/Common/RestCollectionDefinition.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NKZSoft.Catalog.Service.Presentation.Rest.Tests.Common;
-
-[CollectionDefinition(nameof(RestCollectionDefinition))]
-public sealed class RestCollectionDefinition : CoreCollectionDefinition<RestWebApplicationFactory<Program>>
-{
-}
